4141d369ca9a4980a1c246e898add15c8e356fb6
Guilherme Comparotto Pucciarelli <gui.c.pucciarelli@gmail.com>
2026-04-02 17:49:47 -0300

    Atualizando curso de React

--- 06_calculadora_imc/imc_calc/src/components/ImcCalc.jsx
+++ 06_calculadora_imc/imc_calc/src/components/ImcCalc.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 import "./ImcCalc.css";
 import Button from "./Button";
 
-const ImcCalc = () => {
-  return <div id="calc-container">
+const ImcCalc = ({calcImc}) => {
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+  
+    const clearForm = (e) => {
+        e.preventDefault();
+        setHeight("");
+        setWeight("");
+    };
+
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, "");
+    };
+
+    const handleHeightChange = (e) => {
+        const updateValue = validDigits(e.target.value);
+
+        setHeight(updateValue);
+    };
+
+    const handleWeightChange = (e) => {
+        const updateValue = validDigits(e.target.value);
+
+        setWeight(updateValue);
+    };
+
+    return (
+    <div id="calc-container">
         <h2>Calculadora de IMC</h2>
         <form id="imc-form">
             <div className="form-inputs">
@@ -12,7 +40,10 @@ const ImcCalc = () => {
                         type="text" 
                         name="height" 
                         id="height" 
-                        placeholder="Exemplo 1.75"/>
+                        placeholder="Exemplo 1.75"
+                        onChange={(e) => handleHeightChange(e)}
+                        value={height}
+                        />
                 </div>
                 <div className="form-control">
                     <label htmlFor="weight">Peso:</label>
@@ -20,16 +51,22 @@ const ImcCalc = () => {
                         type="text" 
                         name="weight" 
                         id="weight" 
-                        placeholder="Exemplo 70,5"/>
+                        placeholder="Exemplo 70,5"
+                        onChange={(e) => handleWeightChange(e)}
+                        value={weight}/>
                 </div>
             </div>
 
             <div className="action-control">
-                <Button id="calc-btn" text="Calcular"/>
-                <Button id="clear-btn" text="Limpar"/>
+                <Button 
+                    id="calc-btn" 
+                    text="Calcular" 
+                    action={(e) => calcImc(e, height, weight)}/>
+                <Button id="clear-btn" text="Limpar" action={clearForm}/>
             </div>
         </form>
     </div>
+  )
 }
 
 export default ImcCalc
