7e3be79c496380225997f99220eaec550daa1015
Guilherme Comparotto Pucciarelli <gui.c.pucciarelli@gmail.com>
2026-04-14 17:31:28 -0300

    Mais um projeto finalizado em React

---
+++ 11_countdown/src/components/Title.jsx
@@ -0,0 +1,9 @@
+import "../components/Title.css"
+
+const Title = ({title, eventColor}) => {
+  return (
+    <h1 className="title" style={{color: eventColor}}>{title}</h1>
+  )
+}
+
+export default Title
